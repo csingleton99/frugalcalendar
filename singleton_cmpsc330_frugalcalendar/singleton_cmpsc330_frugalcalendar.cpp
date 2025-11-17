f7b5340f012bc636798b46fb52ace303d3d0dbaf
Emmett Kane <112571734+EmmettKane@users.noreply.github.com>
2025-11-17 09:38:27 -0500

    v 0.0
    File created, requirements outlined

---
+++ singleton_cmpsc330_frugalcalendar/singleton_cmpsc330_frugalcalendar.cpp
@@ -0,0 +1,12 @@
+// chris singleton, cmpsc 330, Ganther, November 17th, 2025
+// This program is designed to calculate the next year for which all the days of a year will align with their weekdays and numbers, day-of-month
+//
+
+
+#include <iostream>
+
+int main()
+{
+    // Each non-leap year, the day-of-the-week increments by one, looping at Saturday, except on leap years, where it increments by two
+    std::cout << "Hello World!\n";
+}
